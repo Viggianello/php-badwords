9b22da67ebd8f602b521e5352ba60304e651720a
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-06-15 15:03:49 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,6 @@
+<!--Si tratta di un censuratore rudimentale: predisponete una variabile contenente un breve testo, poi tramite le funzioni sulle stringhe di php censurate una "badword" che viene passata in GET con *** (3 simboli di asterico).
+Stampate a schermo il testo censurato all'interno di un paragrafo e la lunghezza del paragrafo stesso.
+Nome repo: php-badwords (edited) -->
+<?php
+Ciao
+?>
